dbc30645b7d73b03bb2307cb4d78b79ada597ed4
Anthony Luong <AnthonySiLuong@gmail.com>
2021-09-17 17:32:40 -0700

    TBA Teams page

--- src/TeamsPage/TeamsPage.js
+++ src/TeamsPage/TeamsPage.js
@@ -1,16 +1,10 @@
 import React from 'react';
-//to use css styling, import the file here
-// the ./ means go back one level, so we're in this doc <name>.js, going back we're in the folder
-//    so by doing that we can access all the files in the folder
-//    to go back to the overall src file, we could do ../
 import './TeamsPage.css';
 
 function TeamsPage() {
   return (
-    //THIS IS WHERE YOU PUT ALL THE HTML
-    //  dont delete this outer div, idk why it doesnt work without it
-    <div className="TeamsPage">
-      <h1>Current Team</h1>
+    <div className="TeamPage">
+      <h1>Teams TBA</h1>
     </div>
   );
 }
